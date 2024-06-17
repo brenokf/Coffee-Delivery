7cac07abbe325343f15158a1c93344f82e9ea1c9
Breno Melo Mendes <breno.mm@kyndryl.com>
2024-06-16 21:56:14 -0400

    move handleCounterClickPlus from useContext

--- src/pages/Checkout/CoffeCard/index.tsx
+++ src/pages/Checkout/CoffeCard/index.tsx
@@ -14,104 +14,88 @@ import {
   TotalPurchase,
   TotalPurchaseAndDelivery,
 } from './styles'
-import ExpressoTradicional from '../../../assets/coffees/Type=Expresso.svg'
-import Latte from '../../../assets/coffees/Type=Latte.svg'
-import { Minus, Plus, Trash } from 'phosphor-react'
-export function CoffeeCard() {
-  function handleCounterClickPlus(e) {
-    console.log(e)
-    const parentDiv = e.currentTarget.parentNode
-    return parentDiv.querySelector('input[type=number]').stepUp()
-  }
 
-  function handleCounterClickDown(e) {
-    const parentDiv = e.currentTarget.parentNode
-    return parentDiv.querySelector('input[type=number]').stepDown()
-  }
+import { Minus, Plus, Trash } from 'phosphor-react'
+import { useContext, useEffect } from 'react'
+import { CoffePropries, MarketContext } from '../../../contexts/Marketcontext'
+
+export function CoffeeCard() {
+  const {
+    handleCounterClickDown,
+    handleCounterClickPlus,
+    checkout,
+    costFreight,
+    handleUpdateValue,
+    totalAllPurchases,
+    convertedCost,
+    grandTotalOfPurchase,
+    handleRemoveSpecificCoffee,
+  } = useContext(MarketContext)
 
   return (
     <FrameContainer>
       <h4>Cafés selecionados</h4>
       <CoffeeCardContainer>
-        <CoffeeCardItem>
-          <InfoContainer>
-            <img src={ExpressoTradicional} alt="icon" />
-            <DetailsContainer>
-              <p>Expresso Tradicional</p>
-              <Actions>
-                <BuyCounter>
-                  <CounterButton
-                    onClick={(e) => {
-                      handleCounterClickDown(e)
-                    }}
-                  >
-                    <Minus size={14} weight="bold" />
-                  </CounterButton>
-                  <input min="0" name="quantity" value="1" type="number" />
-                  <CounterButton
-                    onClick={(e) => {
-                      handleCounterClickPlus(e)
-                    }}
-                  >
-                    <Plus size={14} weight="bold" />
-                  </CounterButton>
-                </BuyCounter>
-                <RemoveItem>
-                  <Trash size={16} /> REMOVER
-                </RemoveItem>
-              </Actions>
-            </DetailsContainer>
-          </InfoContainer>
-          <p>R$9,90</p>
-        </CoffeeCardItem>
-        <hr />
-        <CoffeeCardItem>
-          <InfoContainer>
-            <img src={Latte} alt="icon" />
-            <DetailsContainer>
-              <p>Latte</p>
-              <Actions>
-                <BuyCounter>
-                  <CounterButton
-                    onClick={(e) => {
-                      handleCounterClickDown(e)
-                    }}
-                  >
-                    <Minus size={14} weight="bold" />
-                  </CounterButton>
-                  <input min="0" name="quantity" value="1" type="number" />
-                  <CounterButton
-                    onClick={(e) => {
-                      handleCounterClickPlus(e)
-                    }}
-                  >
-                    <Plus size={14} weight="bold" />
-                  </CounterButton>
-                </BuyCounter>
-                <RemoveItem>
-                  <Trash size={16} /> REMOVER
-                </RemoveItem>
-              </Actions>
-            </DetailsContainer>
-          </InfoContainer>
-          <p>R$9,90</p>
-        </CoffeeCardItem>
+        {checkout.map((coffee) => {
+          return (
+            <CoffeeCardItem key={coffee.id}>
+              <InfoContainer>
+                <img src={coffee.image} alt="" />
+                <DetailsContainer>
+                  <p>{coffee.title}</p>
+                  <Actions>
+                    <BuyCounter>
+                      <CounterButton
+                        onClick={(e) => {
+                          handleCounterClickDown(e, coffee)
+                        }}
+                      >
+                        <Minus size={14} weight="bold" />
+                      </CounterButton>
+                      <input
+                        min="0"
+                        name="quantity"
+                        value={coffee.quantity}
+                        type="number"
+                      />
+                      <CounterButton
+                        onClick={(e) => {
+                          handleCounterClickPlus(e, coffee)
+                        }}
+                      >
+                        <Plus size={14} weight="bold" />
+                      </CounterButton>
+                    </BuyCounter>
+                    <RemoveItem
+                      onClick={() => {
+                        handleRemoveSpecificCoffee(coffee.id)
+                      }}
+                    >
+                      <Trash size={16} /> REMOVER
+                    </RemoveItem>
+                  </Actions>
+                </DetailsContainer>
+              </InfoContainer>
+              <p>{`${convertedCost(coffee.total)}`}</p>
+            </CoffeeCardItem>
+          )
+        })}
         <hr />
         <TotalPurchaseAndDelivery>
           <TotalPurchase>
             <span>Total Itens</span>
-            <p>R$29,70</p>
+            <p>{`${totalAllPurchases()}`}</p>
           </TotalPurchase>
           <DeliveryFee>
             <span>Entrega</span>
-            <p>R$3,50</p>
+            <p>{`${convertedCost(costFreight)}`}</p>
           </DeliveryFee>
           <Total>
             <span>Total</span>
-            <p>R$33,20</p>
+            <p>{`${grandTotalOfPurchase(costFreight)}`}</p>
           </Total>
         </TotalPurchaseAndDelivery>
-        <ConfirmOrder>CONFIRMAR PEDIDO</ConfirmOrder>
+        <ConfirmOrder onClick={() => {}}>CONFIRMAR PEDIDO</ConfirmOrder>
       </CoffeeCardContainer>
     </FrameContainer>
   )
